8201d328ed399af361824d58ff38858848314d2a
Urs Gerber <ug.gerber@gmail.com>
2016-10-03 10:07:27 +0200

    Use SwissDRG IT Naming convention for workspaces

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD:/opt/grouperserve -p 4567:4567 --workdir /opt/grouperserve --name=grouperserve --detach=true java:8 java -cp build/libs/grouperserve-0.1.1.jar ch.eonum.grouperserve.GrouperServe
+docker run -v $PWD:/opt/grouperserve -p 4567:4567 --workdir /opt/grouperserve --name=grouperserve --detach=true java:8 java -cp build/libs/grouperserve-0.1.2.jar ch.eonum.grouperserve.GrouperServe
